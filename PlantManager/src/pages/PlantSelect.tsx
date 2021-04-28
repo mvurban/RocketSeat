4628d5d18950e28256e10a1d067bf90e67fe2583
mvurban <mvurban@mvurban.eti.br>
2021-04-28 19:32:38 -0300

    ajustes

--- PlantManager/src/pages/PlantSelect.tsx
+++ PlantManager/src/pages/PlantSelect.tsx
@@ -1,13 +1,37 @@
 
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { View, Text, StyleSheet, SafeAreaView, Button } from 'react-native'
 import colors from '../styles/colors'
 import {Perfil} from '../components/Perfil'
 import fonts from '../styles/fonts'
-import { ScrollView } from 'react-native-gesture-handler'
+import { FlatList, ScrollView } from 'react-native-gesture-handler'
 import ButtonAmbiente from '../components/ButtonAmbiente'
+import api from '../services/api'
+
+interface ambientesProps {
+   key: string,
+   title: string,
+}
 
 export function PlantSelect() {
+
+   const [ambientes, setAmbientes] = useState<ambientesProps[]>([]);
+
+   useEffect(() => {
+      async function feachAmbientes(){
+         const {data} = await api.get('plants_environments');
+         //setAmbientes(data)
+         setAmbientes([
+            {
+               key:'all',
+               title: 'Todos'
+            }, ...data])
+
+      }
+      feachAmbientes()
+      
+   }, [])
+
    return (
       <SafeAreaView style={styles.container}>
          <Perfil texto1='Olá,' texto2='Marcelo' avatar = ''></Perfil>
@@ -16,6 +40,17 @@ export function PlantSelect() {
             <Text style={styles.subTitulo}>você quer colocar sua planta?</Text>
          </View>
          <View style={styles.containerBotoesAmbiente}>
+            <FlatList 
+               style={styles.ScrollViewBotoesAmbiente}
+               data={ambientes} 
+               renderItem={({item}) => (
+                  <ButtonAmbiente titulo={item.title} onPress={()=>{}}></ButtonAmbiente>               
+               )}
+               horizontal
+               showsHorizontalScrollIndicator = {false}
+            ></FlatList>            
+
+            {/* 
             <ScrollView style={styles.ScrollViewBotoesAmbiente} horizontal={true} showsHorizontalScrollIndicator={false} >               
                <ButtonAmbiente titulo='Sala' onPress={()=>{}}></ButtonAmbiente>               
                <ButtonAmbiente titulo='Quarto' onPress={()=>{}} destaque={true}></ButtonAmbiente>               
@@ -23,6 +58,8 @@ export function PlantSelect() {
                <ButtonAmbiente titulo='Banheiro' onPress={()=>{}}></ButtonAmbiente>               
                <ButtonAmbiente titulo='Sacada' onPress={()=>{}}></ButtonAmbiente>               
             </ScrollView>
+             */}
+         
          </View>
          <View style={styles.containerPlantas}>
 
