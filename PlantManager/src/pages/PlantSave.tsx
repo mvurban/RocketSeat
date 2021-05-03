c6b364cd176eceb3a6e35c8b444bcdccf1b1f794
mvurban <mvurban@mvurban.eti.br>
2021-05-03 18:01:43 -0300

    Create PlantSave.tsx

---
+++ PlantManager/src/pages/PlantSave.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import {SvgFromUri} from  'react-native-svg'
+
+export default function PlantSave() {
+   return (
+      <View>
+         <Text></Text>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container:{
+
+   }
+})
